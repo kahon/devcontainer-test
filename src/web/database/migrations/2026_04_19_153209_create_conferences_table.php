b3eb63e1e0807e8fd2f47052742e654467b8fac5
matsumoto@startup-plus.com <matsumoto@startup-plus.com>
2026-04-20 00:36:12 +0900

    カンファレンス関連 マイグレーション作成

---
+++ src/web/database/migrations/2026_04_19_153209_create_conferences_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('conferences', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->timestamp('scheduled_at');
+            $table->string('location')->nullable();
+            $table->foreignId('created_by')->constrained('users');
+            $table->foreignId('updated_by')->nullable()->constrained('users');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('conferences', function (Blueprint $table) {
+            $table->dropForeign(['created_by']);
+            $table->dropForeign(['updated_by']);
+        });
+        Schema::dropIfExists('conferences');
+    }
+};
